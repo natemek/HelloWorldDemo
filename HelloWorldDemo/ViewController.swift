9055ed5321859f997cee0663688cf67f4e3548da
natemek <natnaelmek@gmail.com>
2019-12-25 00:50:00 -0500

    First Commit

--- HelloWorldDemo/ViewController.swift
+++ HelloWorldDemo/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    var backgroundColor: UIColor!
+    var oldText: String!
+    var oldTextColor: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        backgroundColor = view.backgroundColor
+        oldText = textLabel.text
+        oldTextColor = textLabel.textColor
     }
 
-
+    @IBAction func didTapButton(_ sender: Any) {
+        textLabel.textColor = UIColor.orange
+    }
+    
+    @IBAction func didTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.blue
+    }
+    
+    @IBAction func didTapTextButton(_ sender: Any) {
+        textLabel.text = textField.text
+        textField.text = ""
+    }
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+        textLabel.text = oldText
+        textLabel.textColor = oldTextColor
+        view.backgroundColor = backgroundColor
+    }
+    
 }
 
